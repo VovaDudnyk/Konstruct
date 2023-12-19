41591a5806e2a7a0c0ddcb233cbb5551f13b9ca4
VovaDudnyk <153094163+VovaDudnyk@users.noreply.github.com>
2023-12-19 11:40:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.scroll').addEventListener('click', function() {
+    const mainAboutSection = document.querySelector('.main-about');
+    if (mainAboutSection) {
+        window.scrollTo({
+            top: mainAboutSection.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+});
