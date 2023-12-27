78946b8fa339cae8401db505e7fbb95369cc7ef5
VovaDudnyk <dudnvova@gmail.com>
2023-12-27 14:09:13 +0200

    form fix

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ document.querySelector(".scroll-button").addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+const popup = document.querySelector("#popup");
+const body = document.body;
+const menu = document.querySelector("#menu").cloneNode(1);
+humb.addEventListener("click", hambHandler);
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  humb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+
+  renderPopup();
+}
+function renderPopup() {
+  popup.appendChild(menu);
+}
+const links = Array.from(menu.children);
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+function closeOnClick() {
+  popup.classList.remove("open");
+  humb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
